2aba97a663ec93cc154fef62514c3c1b19ef45d6
Stephen Wright <scw248@gmail.com>
2019-01-05 23:54:14 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,6 +8,8 @@
 
 def select_winner(passengers)
   passengers.each do |key, value|
-    if key == "suite_a" && value.start_with?
+    if key == "suite_a" && value.start_with?("A", "a")
+      return value
+    end
 end
 end
